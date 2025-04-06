d4638c04fad2aabcbf6d68357b4b32c66cca655a
danielrangel <juan.daniel.rangel.avila@gmail.com>
2025-04-06 16:31:25 -0600

    db modified

--- test_saveme/settings/base.py
+++ test_saveme/settings/base.py
@@ -83,13 +83,23 @@ WSGI_APPLICATION = "test_saveme.wsgi.application"
 
 # Database
 # https://docs.djangoproject.com/en/5.1/ref/settings/#databases
-
-DATABASES = {
-    "default": {
-        "ENGINE": "django.db.backends.sqlite3",
-        "NAME": os.path.join(BASE_DIR, "db.sqlite3"),
+if os.getenv('DJANGO_DEV', 'false') == 'true':
+    DATABASES = {
+        "default": {
+            "ENGINE": "django.db.backends.sqlite3",
+            "NAME": os.path.join(BASE_DIR, "db.sqlite3"),
+        }
     }
-}
+else:
+    DATABASES = {
+        'default': {
+            'ENGINE': 'django.db.backends.postgresql',
+            'NAME': 'save_db',
+            'USER': 'daniel',
+            'PASSWORD': 'titanfaL1?',
+            'HOST': 'localhost',
+            'PORT': '5432',
+        }
 
 
 # Password validation
